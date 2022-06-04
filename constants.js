2ff29bd352a20cc89845e6d9a07cd5295d5be292
MonoGot <kirati.samat@gmail.com>
2022-06-04 16:11:42 +0700

    fix: METADATA_URL

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x0721754cED3c1A87c0e353a0DD5ec5019cb7Efb9";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-smart-website.vercel.app/api/";
 
 module.exports = {
   WHITELIST_CONTRACT_ADDRESS,
